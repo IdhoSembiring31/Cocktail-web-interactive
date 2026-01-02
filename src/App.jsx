734f3fdaae53d061a7d5c5e9dc6ed6b078867b09
idho dev <idhosembiring6@gmail.com>
2026-01-02 17:14:28 +0700

    implement Contact Section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 function App() {
   return (
     <main>
@@ -17,6 +18,7 @@ function App() {
       <About/>
       <Art/>
       <Menu/>
+      <Contact/>
     </main>
   )
 }
